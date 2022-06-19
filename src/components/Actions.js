6e1df58d266a64fb27f9fdf6748aaed0eb875f1f
Ornella-e <orneescaladacorsi@gmail.com>
2022-06-19 16:54:58 +0200

    done

---
+++ src/components/Actions.js
@@ -0,0 +1,12 @@
+function Actions (props){
+    return(
+        <div className="actions">
+        <i class="far fa-comment">{props.actions}</i>
+        <i class="fas fa-retweet">{props.actions}</i>
+        <i class="far fa-heart">{props.actions}</i>
+        <i class="fas fa-share">{props.actions}</i>
+      </div>   
+    );
+}
+
+export default Actions;
